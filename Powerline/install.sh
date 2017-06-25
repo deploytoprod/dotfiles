c0b8d61695c310a488d749846de7c1c8068692d5
Rafael Lopes <dev@rafalop.es>
2017-06-25 14:29:07 -0400

    Doing sudo on script and correctly placing the symlinks

--- Powerline/install.sh
+++ Powerline/install.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 # No testing, no error handling, no nothing. Pray and it will work.
+# Trust me.
 
 brew install python
 brew install vim --env-std --override-system-vim 
-pip install https://github.com/Lokaltog/powerline/tarball/develop
+sudo pip install https://github.com/Lokaltog/powerline/tarball/develop
 echo "source /usr/local/lib/python2.7/site-packages/powerline/bindings/vim/plugin/powerline.vim" >> ~/.vimrc
 echo "set laststatus=2" >> ~/.vimrc
 echo "source /usr/local/lib/python2.7/site-packages/powerline/bindings/zsh/powerline.zsh" >> ~/.zshrc
-pip install powerline-gitstatus
-rm -fR /usr/local/lib/python2.7/site-packages/powerline/config_files/colorschemes/default.json
-rm -fR /usr/local/lib/python2.7/site-packages/powerline/config_files/themes/shell/default.json
-ln -s $(pwd)/config_files/colorschemes/default.json /usr/local/lib/python2.7/site-packages/powerline/config_files/colorschemes/default.json
-ln -s $(pwd)/config_files/themes/shell/default.json /usr/local/lib/python2.7/site-packages/powerline/config_files/themes/shell/default.json
+sudo pip install powerline-gitstatus
+sudo rm -fR /usr/local/lib/python2.7/site-packages/powerline/config_files/colorschemes/default.json
+sudo rm -fR /usr/local/lib/python2.7/site-packages/powerline/config_files/themes/shell/default.json
+sudo ln -s $(pwd)/config_files/colorschemes/shell/default.json /usr/local/lib/python2.7/site-packages/powerline/config_files/colorschemes/shell/default.json
+sudo ln -s $(pwd)/config_files/themes/shell/default.json /usr/local/lib/python2.7/site-packages/powerline/config_files/themes/shell/default.json
