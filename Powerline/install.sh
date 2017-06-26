d569511dc810d63ab97475cf510091dd514e5cf1
Rafael Lopes <dev@rafalop.es>
2017-06-26 18:32:02 -0400

    Fixing the rm path

--- Powerline/install.sh
+++ Powerline/install.sh
@@ -10,7 +10,7 @@ echo "source /usr/local/lib/python2.7/site-packages/powerline/bindings/vim/plugi
 echo "set laststatus=2" >> ~/.vimrc
 echo "source /usr/local/lib/python2.7/site-packages/powerline/bindings/zsh/powerline.zsh" >> ~/.zshrc
 sudo pip install powerline-gitstatus
-sudo rm -fR /usr/local/lib/python2.7/site-packages/powerline/config_files/colorschemes/default.json
+sudo rm -fR /usr/local/lib/python2.7/site-packages/powerline/config_files/colorschemes/shell/default.json
 sudo rm -fR /usr/local/lib/python2.7/site-packages/powerline/config_files/themes/shell/default.json
 sudo ln -s $(pwd)/config_files/colorschemes/shell/default.json /usr/local/lib/python2.7/site-packages/powerline/config_files/colorschemes/shell/default.json
 sudo ln -s $(pwd)/config_files/themes/shell/default.json /usr/local/lib/python2.7/site-packages/powerline/config_files/themes/shell/default.json
